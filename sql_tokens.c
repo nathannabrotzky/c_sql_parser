bd9943c7bf435700b8c844ca670abb89531e6337
Nathan Nabrotzky <16818427+nathannabrotzky@users.noreply.github.com>
2025-12-08 22:34:04 -0700

    Update sql_tokens.c

--- sql_tokens.c
+++ sql_tokens.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include "sql_tokens.h"
 
 void toUppercase(char *str) {
     int i = 0;
@@ -46,42 +47,74 @@ bool is_keyword(char buffer[100]) {
   return false;
 };
 
-typedef struct {
-  char type[32];
-  char value[128];
-} sqlToken;
-
-int main(void) {
-    FILE *filePointer = fopen("test.sql", "r");
-    fseek(filePointer, 0, SEEK_END);
-    long int size = ftell(filePointer);
-    fseek(filePointer, 0, SEEK_SET);
-    char buffer[100];
-    int offset = 0;
-    int layer = 0;
-    for (long int i = 0; i < size; i++) {
-      char c = fgetc(filePointer);
-      if (c == '(') {
-        layer++;
-      } else if (c == ')') {
-        layer--;
-      }
-      bool stopFlag = is_stop(c);
-      bool puncFlag = is_punctuation(c);
-      if ((stopFlag || puncFlag) && ((c == '(' && layer == 1)|| layer == 0)) {
-        if (buffer[0] != '\0') {
-          sqlToken newToken;
-          strcpy(newToken.type,(is_keyword(buffer)? "KEYWORD" : "IDENTIFIER"));
-          strcpy(newToken.value,buffer);
-          printf("%s %s\n",newToken.type, newToken.value);
-        }
-        buffer[0] = '\0';
-        offset = i + 1;
-        continue;
-      }
-      buffer[i - offset] = c;
-      buffer[i - offset + 1] = '\0';
+sqlToken* sql_lexer(const char *filepath, int *count) {
+  sqlToken *result = NULL;
+  int current_size = 0;
+  int capacity = 0;
+  FILE *filePointer = fopen(filepath, "r");
+  if (filePointer == NULL) {
+    printf("Error opening the file.\n");
+    return 1;
+  }
+  if (fseek(filePointer, 0, SEEK_END) == -1) {
+    printf("Error finding end of file.\n");
+    return 1;
+  }
+  long int size = ftell(filePointer);
+  if (size == -1) {
+    printf("Error getting the size of file.\n");
+    return 1;
+  }
+  if (fseek(filePointer, 0, SEEK_SET) == -1) {
+    printf("Error finding beginning of file.\n");
+    return 1;
+  }
+  char buffer[100];
+  int offset = 0;
+  int layer = 0;
+  for (long int i = 0; i < size; i++) {
+    char c = fgetc(filePointer);
+    if (c == EOF) {
+      printf("Error getting next character.\n");
+      return 1;
     }
-    fclose(filePointer);
-    return 0;
+    if (c == '(') {
+      layer++;
+    } else if (c == ')') {
+      layer--;
+    }
+    bool stopFlag = is_stop(c);
+    bool puncFlag = is_punctuation(c);
+    if ((stopFlag || puncFlag) && ((c == '(' && layer == 1)|| layer == 0)) {
+      if (buffer[0] != '\0') {
+        sqlToken newToken;
+        strcpy(newToken.type,(is_keyword(buffer)? "KEYWORD" : "IDENTIFIER"));
+        strcpy(newToken.value,buffer);
+        if (current_size >= capacity) {
+          capacity = (capacity == 0) ? 2 : capacity * 2; 
+          int *temp_arr = (int *)realloc(result, capacity * sizeof(sqlToken));
+          if (temp_arr == NULL) {
+            fprintf(stderr, "Memory reallocation failed!\n");
+            free(result);
+            return 1;
+          }
+          result = temp_arr;
+          printf("Reallocated to capacity: %zu\n", capacity);
+        }
+        result[current_size++] = newToken;
+        printf("%s %s\n",newToken.type, newToken.value);
+      }
+      buffer[0] = '\0';
+      offset = i + 1;
+      continue;
+    }
+    buffer[i - offset] = c;
+    buffer[i - offset + 1] = '\0';
+  }
+  *count = current_size;
+  if (fclose(filePointer) == EOF) {
+    print("Error closing the file.\n");
+    return 1;
+  }
+  return result;
 }
